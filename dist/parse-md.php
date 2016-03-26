858564707bebbe9fa46034a659e70754c136e5f9
Linus Metzler <linus.metzler@gmail.com>
2016-03-26 15:57:19 +0100

    fully automatic creation of hand-in files
    it needs quite a few dependencies, so it might not work on your machine

---
+++ dist/parse-md.php
@@ -0,0 +1,18 @@
+<?php
+
+$file = file_get_contents('pdf-skeleton.md');
+
+$file = preg_replace_callback('/!include (.*)/', function($matches) {
+    return '#'.file_get_contents(trim($matches[1]));
+}, $file);
+
+$file = preg_replace_callback('/!loop-images (.*)/', function($matches) {
+    $files = glob(trim($matches[1]));
+    natsort($files);
+
+    return implode("\n\n", array_map(function($v) {
+        return 'Instance '.pathinfo($v, PATHINFO_FILENAME)."\n".'<img src="'.$v.'">';
+    }, $files));
+}, $file);
+
+file_put_contents('pdf.md', $file);
